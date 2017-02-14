83c14bf77b5e97120bf18500c9482f7cc9b666d6
Andreas Leha <andreas.leha@med.uni-goettingen.de>
2017-02-14 15:21:14 +0000

    version 0.0.1.9

---
+++ R/data.R
@@ -0,0 +1,20 @@
+##' Paper Planes.
+##'
+##' Distance Recordings from a Paper Plane Folding/Flying Experiment
+##'
+##' Visitors of a science fair (the 3. 'Nacht des Wissens' in
+##' Goettingen/Germany \url{http://www.ndw.uni-goettingen.de/}) had the
+##' opportunity to throw a paper plane -- either folding one
+##' themselves or re-using an existing one.
+##'
+##' @encoding UTF-8
+##' @format A data frame with eight variables:
+##' \code{id},
+##' \code{hour} (the hour of the day),
+##' \code{person},
+##' \code{gender},
+##' \code{age},
+##' \code{plane},
+##' \code{paper} (two types: 80g and 120g),
+##' \code{distance} (recorded in meter).
+"paperplanes"
